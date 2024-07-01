0bb864a0ee1bd983118f12e3cd23858d0f36dc7a
KeK <cuddlelyholly@naver.com>
2024-07-01 12:27:36 +0900

    :memo: day 14 package 1,2,3

--- src/day14/Inheritance/sec03/exam01/ComputerExample.java
+++ src/day14/Inheritance/sec03/exam01/ComputerExample.java
@@ -1,5 +1,7 @@
 package day14.Inheritance.sec03.exam01;
 
+import day14.Inheritance.sec05.package2.C;
+
 public class ComputerExample {
 
   public static void main(String[] args) {
@@ -13,5 +15,14 @@ public class ComputerExample {
     double area = calculator2.areaCircle(20);
     System.out.printf("원 면적 : %.2f", area);
 
+    int radius = 10;
+    Calculator calculator = new Calculator();
+    System.out.println("원의 면적 : " + calculator.areaCircle(radius));
+
+    System.out.println("======UPCasting======");
+    Calculator calculator1 = new Computer();
+    double area = calculator1.areaCircle(10);
+    System.out.printf("원의 면적 : %.2f", area);
+    
   }
 }
